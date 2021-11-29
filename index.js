4cfc1dde7b7d613a61774b50d0d7589cf6519950
sofiaversoza <sofversoza@gmail.com>
2021-11-29 11:37:47 -0700

    commit tree 35f3610756bd5eefe563712f860696fc3134c5c2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, str) {
+    return array.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === str.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(array, testString) {
+    return array.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(array, strName) {
+    return array.filter( record => record.name === strName)
+  }
